679abff3717043de5b6be72b1a9fc04a426ed424
lauracazorla <lauracaz1@hotmail.com>
2023-11-15 09:53:48 +0100

    Updated javadoc documentation

---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Assessment","l":"DTOSIAssessment"},{"p":"Assessment","l":"DTOSICategory"},{"p":"Assessment","l":"SIAssessment"}];updateSearchResults();
--- src/main/java/Assessment/SIAssessment.java
+++ src/main/java/Assessment/SIAssessment.java
@@ -12,18 +12,17 @@ import java.util.Map;
 public class SIAssessment {
 
     /**
-     *
-     * @param IDSI: ID of the Strategic Indicator to be assessed. Has to coincide with its corresponding node's name in
-     *           the BN file to use (BNFile). Example: productquality
-     * @param factorStates: Map containing the states of the SI's parent nodes. Being the keys the IDs of the parent
+     * @param IDSI ID of the Strategic Indicator to be assessed. Has to coincide with its corresponding node's name in
+     *           the BN file to use (BNFile). Example: productquality.
+     * @param factorStates Map containing the states of the SI's parent nodes. Being the keys the IDs of the parent
      *                    nodes and the values being the state itself. IDs and states need to be consistent with the
-     *                    BN file in use (BNFile). Example: "CodeQuality" : "VeryLow"
-     *                                                      "Stability" : "Medium"
-     *                                                      "TestingStatus" : "High"
-     * @param BNFile: File containing the BN to use for the assessment, in DNE format
-     * @return Assessment of the SI: ArrayList of DTOSICategory, each one containing the ID of the SI category
+     *                    BN file in use (BNFile). Example: "CodeQuality" : "VeryLow",
+     *                                                      "Stability" : "Medium",
+     *                                                      "TestingStatus" : "High".
+     * @param BNFile File containing the BN to use for the assessment, in DNE format.
+     * @return Assessment of the SI - ArrayList of DTOSICategory, each one containing the ID of the SI category
      * (idSICategory) and its associated assessed probability (probSICategory).
-     * @throws Exception
+     * @throws Exception Thrown by a method in the Unbbayes library
      */
     public static DTOSIAssessment AssessSI(String IDSI, Map<String, String> factorStates, File BNFile) throws Exception {
         BaseIO io = new DneIO();
